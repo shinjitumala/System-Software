80965e49be5fdd4112d7ab8171e332adbb3c36cb
shinjitumala <funny.pig.run@gmail.com>
2018-10-16 17:05:09 +0900

    sleep_secシステムコールの実装　現在スリープロックではなく、スピンロックの実装になってしまっている

--- sysproc.c
+++ sysproc.c
@@ -1,73 +1,53 @@
-#include "types.h"
-#include "x86.h"
-#include "defs.h"
 #include "date.h"
-#include "param.h"
+#include "defs.h"
 #include "memlayout.h"
 #include "mmu.h"
+#include "param.h"
 #include "proc.h"
+#include "types.h"
+#include "x86.h"
 
-int
-sys_fork(void)
-{
-  return fork();
-}
+int sys_fork(void) { return fork(); }
 
-int
-sys_exit(void)
-{
+int sys_exit(void) {
   exit();
-  return 0;  // not reached
+  return 0; // not reached
 }
 
-int
-sys_wait(void)
-{
-  return wait();
-}
+int sys_wait(void) { return wait(); }
 
-int
-sys_kill(void)
-{
+int sys_kill(void) {
   int pid;
 
-  if(argint(0, &pid) < 0)
+  if (argint(0, &pid) < 0)
     return -1;
   return kill(pid);
 }
 
-int
-sys_getpid(void)
-{
-  return myproc()->pid;
-}
+int sys_getpid(void) { return myproc()->pid; }
 
-int
-sys_sbrk(void)
-{
+int sys_sbrk(void) {
   int addr;
   int n;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
+  if (growproc(n) < 0)
     return -1;
   return addr;
 }
 
-int
-sys_sleep(void)
-{
+int sys_sleep(void) {
   int n;
   uint ticks0;
 
-  if(argint(0, &n) < 0)
+  if (argint(0, &n) < 0)
     return -1;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(myproc()->killed){
+  while (ticks - ticks0 < n) {
+    if (myproc()->killed) {
       release(&tickslock);
       return -1;
     }
@@ -79,9 +59,7 @@ sys_sleep(void)
 
 // return how many clock tick interrupts have occurred
 // since start.
-int
-sys_uptime(void)
-{
+int sys_uptime(void) {
   uint xticks;
 
   acquire(&tickslock);
@@ -91,18 +69,132 @@ sys_uptime(void)
 }
 
 // kadai1
-int
-sys_getdate(void)
-{
+int sys_getdate(void) {
   struct rtcdate *dp;
-  if (argptr(0, (char **)&dp, sizeof(dp)) < 0) return -1;
+  if (argptr(0, (char **)&dp, sizeof(dp)) < 0)
+    return -1;
   cmostime(dp);
   return 0;
 }
 
 // kadai1
-int
-sys_sleep_sec(void){
-  // not implemented
+int sys_sleep_sec(void) {
+  int n;
+  struct rtcdate dp0, dp;
+
+  // returns error if the int argument was negative
+  if (argint(0, &n) < 0)
+    return -1;
+
+  cmostime(&dp0);
+  int l = 0;
+  while (1) {
+    l++;
+    l = l % 40;
+    if (l == 0) {
+      cmostime(&dp);
+
+      int delta = 0;
+      // year
+      if (dp.year != dp0.year) {
+        int i;
+        for (i = dp0.year; i != dp.year; i++) {
+          if (i % 4 != 0 || (i % 4 == 0 && i % 100 == 0 && i % 400 != 0)) {
+            delta += 31536000;
+          } else {
+            delta += 31622400;
+          }
+        }
+      }
+      // month
+      if (dp.month != dp0.month) {
+        int year = dp0.year;
+        int i;
+        for (i = dp0.month; i != dp.month;) {
+          switch (i) {
+          case 1:
+          case 3:
+          case 5:
+          case 7:
+          case 8:
+          case 10:
+          case 12:
+            delta += 2678400;
+            break;
+          case 4:
+          case 6:
+          case 9:
+          case 11:
+            delta += 2592000;
+            break;
+          case 2:
+            if (year % 4 != 0 ||
+                (year % 4 == 0 && year % 100 == 0 && year % 400 != 0)) {
+              delta += 2419200;
+            } else {
+              delta += 2505600;
+            }
+            break;
+          }
+
+          if (i == 12) {
+            i = 1;
+            year = dp.year;
+          } else
+            i++;
+        }
+      }
+      // day
+      if (dp0.day != dp.day) {
+        int year = dp0.year;
+        int month = dp0.month;
+        int i;
+        for (i = dp0.day; i != dp.day;) {
+          delta += 86400;
+
+          int max = 0;
+          switch (month) {
+          case 1:
+          case 3:
+          case 5:
+          case 7:
+          case 8:
+          case 10:
+          case 12:
+            max = 31;
+            break;
+          case 4:
+          case 6:
+          case 9:
+          case 11:
+            max = 30;
+            break;
+          case 2:
+            if (year % 4 != 0 ||
+                (year % 4 == 0 && year % 100 == 0 && year % 400 != 0)) {
+              max = 28;
+            } else {
+              max = 29;
+            }
+            break;
+          }
+
+          if (i == max) {
+            year = dp.year;
+            month = dp.month;
+            i = 1;
+          } else {
+            i++;
+          }
+        }
+      }
+      // hour, minute, second
+      delta += (dp.second - dp0.second) + (dp.minute - dp0.minute) * 60 +
+               (dp.hour - dp0.hour) * 3600;
+      if (delta >= n)
+        break;
+    }
+  }
+
   return 0;
 }
